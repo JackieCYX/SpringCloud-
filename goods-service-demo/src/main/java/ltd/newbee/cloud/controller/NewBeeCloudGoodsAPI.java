0e6dbde906e9b9d86b0536e21393a142e7107723
JordanCYX <2338257659@qq.com>
2023-11-18 15:15:51 +0800

    OpenFeign参数传递编码实践

--- goods-service-demo/src/main/java/ltd/newbee/cloud/controller/NewBeeCloudGoodsAPI.java
+++ goods-service-demo/src/main/java/ltd/newbee/cloud/controller/NewBeeCloudGoodsAPI.java
@@ -1,24 +1,70 @@
 package ltd.newbee.cloud.controller;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.util.CollectionUtils;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 public class NewBeeCloudGoodsAPI {
 
     @Value("${server.port}")
-    private String applicationServerPort;// 读取当前应用的启动端口
+    private String applicationServerPort;
 
-    @GetMapping("/goods/{goodsId}")
-    public String goodsDetail(@PathVariable("goodsId") int goodsId) {
+    @GetMapping("/goods/detail")
+    //传递多个参数 参数都是URL参数
+    public String goodsDetailByParams(@RequestParam("sellStatus") int sellStatus, @RequestParam("goodsId") int goodsId) {
+
+        System.out.println("参数如下：sellStatus=" + sellStatus + ",goodsId=" + goodsId);
+
         // 根据id查询商品并返回给调用端
         if (goodsId < 1 || goodsId > 100000) {
-            return "查询商品为空，当前服务的端口号为" + applicationServerPort;
+            return "goodsDetailByParams 查询商品为空，当前服务的端口号为" + applicationServerPort;
         }
-        String goodsName = "商品" + goodsId;
+        String goodsName = "商品" + goodsId + ",上架状态 "+sellStatus;
         // 返回信息给调用端
-        return goodsName + "，当前服务的端口号为" + applicationServerPort;
+        return "goodsDetailByParams " + goodsName + "，当前服务的端口号为" + applicationServerPort;
+    }
+
+    @GetMapping("/goods/listByIdArray")
+    //传递数组类型
+    public String[] listByIdArray(@RequestParam("goodsIds") Integer[] goodsIds) {
+
+        // 根据goodsIds查询商品并返回给调用端
+        if (goodsIds.length < 1) {
+            return null;
+        }
+
+        String[] goodsInfos = new String[goodsIds.length];
+
+        for (int i = 0; i < goodsInfos.length; i++) {
+            goodsInfos[i] = "商品" + goodsIds[i];
+        }
+
+        // 接收参数为数组，返回信息给调用端亦为数组类型
+        return goodsInfos;
+    }
+
+    @GetMapping("/goods/listByIdList")
+    //传递链表类型
+    public List<String> listByIdList(@RequestParam("goodsIds") List<Integer> goodsIds) {
+
+        // 根据goodsIds查询商品并返回给调用端
+        if (CollectionUtils.isEmpty(goodsIds)) {
+            return null;
+        }
+
+        List<String> goodsInfos = new ArrayList<>();
+
+        for (int goodsId : goodsIds) {
+            goodsInfos.add("商品" + goodsId);
+        }
+
+        // 接收参数为链表，返回信息给调用端亦为链表类型
+        return goodsInfos;
     }
 }
