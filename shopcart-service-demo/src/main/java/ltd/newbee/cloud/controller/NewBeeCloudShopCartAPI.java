6259d36b6c6131370eddf6f5d8b7d5a5b002d1df
kallin.chen <kallin.chen@globrobot.com>
2023-11-22 10:32:03 +0800

    购物车服务编写

--- shopcart-service-demo/src/main/java/ltd/newbee/cloud/controller/NewBeeCloudShopCartAPI.java
+++ shopcart-service-demo/src/main/java/ltd/newbee/cloud/controller/NewBeeCloudShopCartAPI.java
@@ -1,15 +1,21 @@
 package ltd.newbee.cloud.controller;
 
+import com.sun.org.apache.xpath.internal.operations.Bool;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
 
 @RestController
 public class NewBeeCloudShopCartAPI {
 
     @Value("${server.port}")
     private String applicationServerPort;// 读取当前应用的启动端口
+
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
 
     @GetMapping("/shop-cart/{cartId}")
     public String cartItemDetail(@PathVariable("cartId") int cartId) {
@@ -20,5 +26,26 @@ public class NewBeeCloudShopCartAPI {
         String cartItem = "购物项" + cartId;
         // 返回信息给调用端
         return cartItem + "，当前服务的端口号为" + applicationServerPort;
+    }
+
+    @GetMapping("/shop-cart/getGoodsId")
+    public int getGoodsId(@RequestParam("cartId") int cartId) {
+        // 根据主键id查询购物表
+        Map<String, Object> cartItemObject = jdbcTemplate.queryForMap("select * from tb_cart_item where cart_id=" + cartId + " limit 1");
+        if (cartItemObject.containsKey("goods_id")) {
+            // 返回商品id
+            return (int) cartItemObject.get("goods_id");
+        }
+        return 0;
+    }
+
+    @DeleteMapping("/shop-cart/{cartId}")
+    public Boolean deleteItem(@PathVariable("cartId") int cartId) {
+        // 删除购物车数据
+        int result = jdbcTemplate.update("delete from tb_cart_item where cart_id=" + cartId);
+        if (result > 0) {
+            return true;
+        }
+        return false;
     }
 }
